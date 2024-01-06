3a7c186f15b6a522ea825bedc0d16c8b9163fe62
minjeong9707 <sally392198@gmail.com>
2024-01-06 11:51:09 +0900

    study: update

--- Day9_Js/index.js
+++ Day9_Js/index.js
@@ -63,10 +63,32 @@ if(myID === inputID){
 }
 */
 
+/*
+//삼항 연산자
+// ? 다음이 참., 위의 if 중첩문이랑 비교하면서 보기
+let myID = 'abc';
+let myPW = '1234';
+const inputId = prompt('아이디를 입력하세요');
+const inputPw = prompt('비밀번호를 입력하세요');
 
+// myID === inputId ? console.log("존재하는 아이디") : console.log("아이디가 틀립니다.");
+
+//중첩도 가능
+myID === inputId
+    ? myPW === inputPw
+        ? console.log('로그인에 성공하였습니다.')
+        : console.log('비밀번호가 틀렸습니다.')
+    : console.log('아이디가 틀립니다.');
+*/
+
+//삼항연산자 실습
+let now = new Date().getHours();
+0 <= now <= 11 ? console.log('오전') : console.log('오후');
+
+/*
 //실습 4,5
 let age = Number(prompt("나이를 입력해주세요"));
-let gender = prompt("성별을 입력해주세요");
+let gender = prompt("성별을 입력해주세요 (남자/여자 중 하나로 입력해주세요.)");
 if(age >=20 ){
   if(gender === "여자"){
     console.log("성인여자");
@@ -102,8 +124,65 @@ else if(age >= 0){
     console.log("남자 유아");
 }
 }
+*/
 
+/*
+// 수정 보완(1)- 실습 5  : 그러나 이것도 반복되는 문구가 너무 많다.
 
+let age1 = Number(prompt("나이를 입력해주세요"));
+let gender1 = prompt("성별을 입력해주세요 (남자/여자 중 하나로 입력해주세요.)");
 
+if(gender1 === "여자"){
+  if(age >=20 ){
+    console.log(`성인 ${gender}`);
+  }else if(age >=17){
+    console.log(`고등학생 ${gender}`);
+  }else if(age >=14){
+    console.log(`중학생 ${gender}`);
+  }else if(age >=8){
+    console.log(`초등학생 ${gender}`);
+  }else {
+    console.log(`유아 ${gender}`);}
+}else{
+  if(age >=20 ){
+    console.log(`성인 ${gender}`);
+  }else if(age >=17){
+    console.log(`고등학생 ${gender}`);
+  }else if(age >=14){
+    console.log(`중학생 ${gender}`);
+  }else if(age >=8){
+    console.log(`초등학생 ${gender}`);
+  }else {
+    console.log(`유아 ${gender}`);}
+}
+*/
 
+/*
+//성별을 기준으로 했을 때 (함수를 이용하자!)
 
+let age = Number(prompt("나이를 입력해주세요"));
+let gender = prompt("성별을 입력해주세요 (남자/여자 중 하나로 입력해주세요.)");
+
+if(gender === "남자"){
+  ageFunc(age, gender);
+}else if(gender === "여자"){
+  ageFunc(age, gender);
+}else{
+  alert('성별을 다시 입력해주세요');
+}
+function ageFunc(age, gender){
+  if(age >= 20 ){
+    console.log(`성인 ${gender}`);
+  }else if(age >= 17){
+    console.log(`고등학생 ${gender}`);
+  }else if(age >= 14){
+    console.log(`중학생 ${gender}`);
+  }else if(age >= 8){
+    console.log(`초등학생 ${gender}`);
+  }else if(age >= 0){
+    console.log(`유아 ${gender}`);
+  }else{
+    alert("나이를 다시 입력해주세요.");
+  }
+}
+*/

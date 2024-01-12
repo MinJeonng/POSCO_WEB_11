a072ad5477f95b7c8ccd1bc221242324f5ff2224
minjeong9707 <sally392198@gmail.com>
2024-01-12 22:48:00 +0900

    update

--- Day13_JQuery/index.js
+++ Day13_JQuery/index.js
@@ -8,10 +8,13 @@ function removeClass() {
     // $('#hi').removeClass(); //속해있는 모든 클래스를 삭제해버림
 }
 function hasClass() {
-    console.log($('#hi').hasClass('font-size')); //true or false 반환
+    console.log($('#hi').hasClass('font-size'));
+    //true or false 반환
+    //해당 클래스가 지정한 요소를 가지고 있는가 boolean 값 으로 반환
 }
 function toggleClass() {
-    $('#hi').toggleClass('bg-color'); //클래스가 있으면 삭제하고, 없으면 삭제
+    $('#hi').toggleClass('bg-color');
+    //클래스가 있으면 삭제하고, 없으면 추가
     //여기선 bg-color가 적용되어 있으면 삭제되고 없으면 추가하고..(add & remove를 같이 하는 것)
 }
 function switchClass() {

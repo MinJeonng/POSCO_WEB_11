60b214e5ecdefdec11962d6d4a5305a39202a195
minjeong <130037564+minjeong9707@users.noreply.github.com>
2024-01-31 11:17:51 +0900

    Nodejs 단순예제

---
+++ Day16_NodeJs_0131/NodeJs/module.js
@@ -0,0 +1,13 @@
+// //하나의 모듈 파일에 하나의 모듈 만들기
+// const a = 10;
+// const b = 100;
+// function connect() {
+//     return a + b;
+// }
+// module.exports = connect; //이 함수를 외부에서 사용할 수 있음
+
+//하나의 모듈 파일에 여러개 모듈 생성
+const c = 100;
+const d = 'd 변수';
+const e = 50;
+module.exports = { c, d, e };

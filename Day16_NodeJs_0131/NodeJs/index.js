c8276c21a1ab4c6981798156b17f6bfc49847f05
minjeong <130037564+minjeong9707@users.noreply.github.com>
2024-02-01 18:03:21 +0900

    Nodejs 단순예제

--- Day16_NodeJs_0131/NodeJs/index.js
+++ Day16_NodeJs_0131/NodeJs/index.js
@@ -2,37 +2,40 @@
 
 // const connect = require('./module.js'); //js는 생략가능
 // console.log(connect.d); //터미널에서 node [파일명] 쓰면 결과 보여줌
-//console.log(connect)
+// console.log(connect);
 
 // const { c, d, e } = require('./module.js');
 // console.log(c, d, e);
 
+/*
 //2. 서버만들기, 서버이벤트
 
 //createServer가 되었는지 확인하고 싶으면 웹에 들어가서 localhost:포트번호 쓰게 되면 확인 가능하다.
 const http = require('http'); //모듈이름 써서 가져오기, http모듈 가져오기
-// const server = http.createServer(function (req, res) {
-//     res.writeHead(202);
-//     res.write('<h1>Hello Nodejs world</h1>');
-//     res.end('<p>End</p>');
-// });
+const server = http.createServer(function (req, res) {
+    res.writeHead(202);
+    res.write('<h1>Hello Nodejs world</h1>');
+    res.end('<p>End</p>');
+});
 //요청할때 들어오는 이벤트
-// server.on('request', function () {
-//     //여기선 get으로 웹 오픈
-//     console.log('요청 이벤트');
-// });
-// server.on('connection', function () {
-//     //새로고침 등의 접속이벤트..
-//     console.log('접속 이벤트');
-// });
+server.on('request', function () {
+    //여기선 get으로 웹 오픈
+    console.log('요청 이벤트');
+});
+server.on('connection', function () {
+    //새로고침 등의 접속이벤트..
+    console.log('접속 이벤트');
+});
 
 //listen : 서버 실행
-// server.listen(8000, function () {
-//     //콜백함수
-//     console.log('8000번 port 실행');
-// });
+server.listen(8000, function () {
+    //콜백함수
+    console.log('8000번 port 실행');
+});
+*/
 
 //3. html 파일전송
+const http = require('http');
 const fs = require('fs');
 const server = http.createServer(function (req, res) {
     //파일 전송하면서 예외처리할때 쓰임

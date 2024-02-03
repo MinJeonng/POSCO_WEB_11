d07ff1387ad4db8343a7875ad967b06f2f75c83d
minjeong <130037564+minjeong9707@users.noreply.github.com>
2024-02-03 12:53:41 +0900

    동적파일 업로드 - axios

--- Day19_fileUpload_0203/express/index.js
+++ Day19_fileUpload_0203/express/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const multer = require('multer');
+const path = require('path'); //폴더와 파일의 경로를 쉽게 조작하도록 제공
 const app = express();
 const PORT = 8000;
 
@@ -15,16 +16,58 @@ const upload = multer({
     dest: 'uploads/',
 });
 
+//multer 세부설정
+const uploadDetail = multer({
+    //storage : 저장할 공간에 대한 정보
+    //diskStorage : 파일을 저장하기 위한 모든 제어기능 제공(파일 저장관련 설정)
+    storage: multer.diskStorage({
+        //destination : 업로드할 파일을 저장할 폴더를 지정
+        destination(req, file, done) {
+            done(null, 'uploads/'); //에러처리 : null, done은 콜백함수
+        },
+        //filename : 파일이름 결정(요청객체, 업로드된 파일 객체, 콜백함수 순서)
+        filename(req, file, done) {
+            //extname() : 확장자를 추출
+            const ext = path.extname(file.originalname);
+            //basename() : 파일이름을 추출(파일의 오리지널명, 확장자) => 확장자를 제외해서 파일이름만 추출
+            const newName = path.basename(file.originalname, ext) + Date.now() + ext;
+            done(null, newName); //콜백함수 호출
+        },
+    }),
+
+    //limits : 파일저장, 용량제한
+    limits: { filesize: 5 * 1024 * 1024 },
+});
+
 //router
 app.get('/', (req, res) => {
     res.render('index');
 });
 //=== 요청, 응답 데이터 처리
 //multer설정이름.하나보내는거라 single.('보내야할 파일이름')
-app.post('/upload', upload.single('userfile'), (req, res) => {
+//(1) single()
+app.post('/upload', uploadDetail.single('userfile'), (req, res) => {
     console.log('file', req.file);
     console.log('title', req.body);
 });
+//(2) array multi
+app.post('/upload/array', uploadDetail.array('userfiles'), (req, res) => {
+    console.log('file', req.file);
+    console.log('title', req.body);
+});
+
+//(3) multi ver2
+app.post('/upload/fields', uploadDetail.fields([{ name1: 'userFile1}' }, { name2: 'userFile2 ' }]), (req, res) => {
+    console.log('file', req.file);
+    console.log('title', req.body);
+});
+
+//(4) 동적
+//single('프론트에서 보내는 key로 했던 파일값 써주면됌') = formData.append('file', file.files[0]); 이부분
+app.post('/upload/axios', uploadDetail.single('file'), (req, res) => {
+    res.send({ file: req.file, title: req.body });
+});
+
 app.listen(PORT, () => {
     console.log(`http://localhost:${PORT}`);
 });

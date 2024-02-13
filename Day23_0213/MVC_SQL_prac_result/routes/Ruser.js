54735e278e1a1ea5218e0cc9d24f1ff2a7ea30b7
minjeong <130037564+minjeong9707@users.noreply.github.com>
2024-02-13 13:16:37 +0900

    MVC패턴 sql연동 회원가입 예제

---
+++ Day23_0213/MVC_SQL_prac_result/routes/Ruser.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const controller = require('../controller/Cuser');
+const { resolveInclude } = require('ejs');
+const router = express.Router();
+
+///localhost:8000/api/user
+//POST /signUp 회원가입
+router.post('/signUp', controller.signUp);
+//POST /login 로그인
+router.post('/login', controller.Clogin);
+//GET /info 회원정보
+router.get('/info/:id', controller.Cinfo);
+//PATCH /update 회원수정
+router.patch('/update', controller.Cupdate);
+//DELETE /delete 삭제
+router.delete('/delete', controller.Cdelete);
+
+module.exports = router;

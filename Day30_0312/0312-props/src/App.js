d37b7ddc28190261be6f34e589d1ab1815314d42
minjeong <130037564+minjeong9707@users.noreply.github.com>
2024-03-12 14:05:05 +0900

    props 예제

---
+++ Day30_0312/0312-props/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import FunctionComponent from './FunctionComponent';
+
+function App() {
+  const name = '코딩온'; //부모
+  //여기에 변수가 있다고 하면 자식한테 값을 전달할 수 잇음
+  return (
+    <div className="App">
+      {/* 부모가 가지고 있는 것을 아래의 자식에게 값을 전달할 수 있다. */}
+      {/* 이 밑에 name = {} 이거가 props */}
+      <FunctionComponent name={name} message={'끝나라'} />
+      {/* 밑에서 name을 따로 안써주면 이 props가 기본값으로 써놓은 값을 가지고 가게됌, 즉, 여기선 홍길동이라고 나옴 */}
+      <FunctionComponent message={'끝나라'} />
+      {/* children은 자식요소에서 사용된다. */}
+      {/* '자식내용' 이라고 쓴 부분은 props.children 인 자리어서 이 함수가 실행되는 파일에서 그냥 children이라고 해도 이 내용을 가지고 간다.*/}
+      <FunctionComponent message={'끝나라'}>자식내용</FunctionComponent>
+    </div>
+  );
+}
+
+export default App;

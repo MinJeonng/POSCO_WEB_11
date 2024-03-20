adc89464f0435c2fb7e0e3e70131db5cf1f8f8c6
minjeong <130037564+minjeong9707@users.noreply.github.com>
2024-03-20 12:15:39 +0900

    scss 예제

--- Day36_0320/0319-styles/src/App.js
+++ Day36_0320/0319-styles/src/App.js
@@ -1,15 +1,27 @@
 import CssModuleComponents from './CssModuleComponents';
 import SassComponents from './SassComponents';
+import StyledComponents from './StyledComponents';
+import PracScssLarva from './PracScssLarva';
 import './styles/App.css';
+import PracScssMixin from './PracScssMixin';
 
 function App() {
   return (
     <div className="App">
       <h1>React Styling</h1>
-      <CssModuleComponents />
+      {/* <CssModuleComponents />
 
       <hr />
       <SassComponents />
+
+      <hr />
+      <StyledComponents /> */}
+
+      {/* <hr /> */}
+      {/* <PracScssLarva /> */}
+
+      <hr />
+      <PracScssMixin />
     </div>
   );
 }
---
+++ Day36_0320/0319-styles/src/PracScssMixin.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/PracScssMixin.scss';
+
+export default function PracScssMixin() {
+  return (
+    <>
+      <div className="circle circle1"></div>
+      <div className="circle circle2"></div>
+      <div className="circle circle3"></div>
+    </>
+  );
+}
